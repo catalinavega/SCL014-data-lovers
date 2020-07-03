b09deeff2257b35b71a7b69c764df680cc478bf2
Catalina Vega <cavs.pho@gmail.com>
2020-07-03 13:42:23 -0400

    03-07-2020 13.45 HRS ACTUALIZACION REPO

--- src/main.js
+++ src/main.js
@@ -3,10 +3,18 @@
 // // import data from './data/lol/lol.js';
 // import data from './data/pokemon/pokemon.js';
 
+// MOSTRAR PÁGINA POKÉDEX
 document.getElementById("firstButton").addEventListener("click", hideAndShow);
 
 function hideAndShow(){
     document.getElementById("firstPage").classList.add("hidden");
     document.getElementById("menu").classList.remove("hidden");
     document.getElementById("secondPage").classList.remove("hidden");
+}
+
+// MOSTRAR Y OCULTAR MENU DESPLEGABLE (SIDEBAR)
+document.querySelector(".toggle-btn").addEventListener("click", showSidebar);
+
+function showSidebar(){
+    document.getElementById("sidebar").classList.toggle("active");
 }
