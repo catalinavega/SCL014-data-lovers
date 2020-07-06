c3108b99520db255d3fe4d8662c027c5d5067fb8
Catalina Vega <cavs.pho@gmail.com>
2020-07-06 16:11:04 -0400

    06-07-2020 16.10 HRS ACTUALIZACION REPO

--- src/main.js
+++ src/main.js
@@ -1,15 +1,8 @@
 import data from "./data/pokemon/pokemon.js";
 
-/* FUNCIÓN PARA TRAER INFO */
-function showPokemonData(){
-    for(let i = 0; i < data.pokemon.length ; i++){
-    console.log(data.pokemon[i].name);
-    }
-};
+let allPokemon = data.pokemon;
+console.log(allPokemon);
 
-showPokemonData();
-
-// MOSTRAR PÁGINA POKÉDEX
 document.getElementById("firstButton").addEventListener("click", hideAndShow);
 
 function hideAndShow(){
@@ -22,7 +15,31 @@ document.querySelector(".toggle-btn").addEventListener("click", showSidebar);
 
 function showSidebar(){
     document.getElementById("sidebar").classList.toggle("active");
-}
+};
+
+/* FUNCIÓN PARA TRAER INFO */
+let allCards = document.querySelector("#allCards");
 
 
+// FUNCIÓN PARA MOSTRAR A LOS POKEMON
+const showPokemonData = () => {
+    let infoPokemon = '';
 
+    for(let i = 0; i < allPokemon.length ; i++){
+        let card = document.createElement("div");
+        if (allPokemon[i].generation.name === 'kanto'){
+            card.setAttribute("class", "kantoCard");
+        } else {
+            card.setAttribute("class", "johtoCard");
+        };
+        infoPokemon = `
+                    <img id="pokemonImage"src="${allPokemon[i].img}">
+                    <p id="pokemonNum">#${allPokemon[i].num}</p>
+                    <p id="pokemonName">${allPokemon[i].name}</p>
+                    <p id="pokemonType">${allPokemon[i].type}</p>`
+        allCards.appendChild(card);
+        card.innerHTML = infoPokemon;
+    }
+};
+
+showPokemonData();
